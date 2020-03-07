0851bac2c9d9ecab05daf33eae220b8e339bcacc
Przemek <pk.korzen1@gmail.com>
2020-03-07 18:59:16 +0100

    refactoring line Component

--- src/app/line/line.component.ts
+++ src/app/line/line.component.ts
@@ -38,17 +38,7 @@ export class LineComponent implements OnInit {
     this.pointCoordinatesService.calculateLocationsCoordinates(width, height);
     const connectionFrom = this.getConnectionCoordinates(this.connectionFrom);
     const connectionTo = this.getConnectionCoordinates(this.connectionTo);
-    if (this.connectionFrom === 'San Francisco' && (this.connectionTo === 'Manila' || this.connectionTo === 'Tokyo')) {
-      this.drawLine2(connectionFrom, connectionTo);
-    } else if (this.connectionFrom === 'Los Angeles' && this.connectionTo === 'Sydney') {
-      this.drawLine2(connectionFrom, connectionTo);
-    } else if ((this.connectionFrom === 'Manila' || this.connectionFrom === 'Tokyo') && this.connectionTo === 'San Francisco') {
-      this.drawLine3(connectionFrom, connectionTo);
-    } else if (this.connectionFrom === 'Sydney' && this.connectionTo === 'Los Angeles') {
-      this.drawLine3(connectionFrom, connectionTo);
-    } else {
-      this.drawLine(connectionFrom, connectionTo);
-    }
+    this.drawLine(connectionFrom, connectionTo, width);
 
   }
 
@@ -62,11 +52,19 @@ export class LineComponent implements OnInit {
     return {height, width};
   }
 
-  private drawLine(connectionFrom: Map<string, number>, connectionTo: Map<string, number>) {
+  private drawLine(connectionFrom: Map<string, number>, connectionTo: Map<string, number>, width: number) {
     const x1: number = connectionFrom.get('left');
     const y1: number = connectionFrom.get('top');
-    const x2: number = connectionTo.get('left');
+    let x2: number;
     const y2: number = connectionTo.get('top');
+
+    if (this.linesGoingAroundGlobeFromWest()) {
+      x2 = -10;
+    } else if (this.linesGoingAroundGlobeFromEast()) {
+      x2 = width - 11;
+    } else {
+      x2 = connectionTo.get('left');
+    }
 
     // the distance between the tow points(for the line div width)
     const distance: number = Math.sqrt( ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
@@ -85,53 +83,17 @@ export class LineComponent implements OnInit {
     this.left = xMid - (distance / 2) + 'px';
     this.transform = 'rotate(' + salopeInDegrees + 'deg)';
   }
-  private drawLine2(connectionFrom: Map<string, number>, connectionTo: Map<string, number>) {
-    const x1: number = connectionFrom.get('left');
-    const y1: number = connectionFrom.get('top');
-    const x2 = -10;
-    const y2: number = connectionTo.get('top');
 
-    // the distance between the tow points(for the line div width)
-    const distance: number = Math.sqrt( ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
-
-    // the mid-point between the two points, we use it as rotation center
-    const xMid: number = (x1 + x2) / 2 + 10;
-    const yMid: number = (y1 + y2) / 2 + 10;
-
-    // get the salope of the line between two points
-    const salopeInRadian: number = Math.atan2(y1 - y2, x1 - x2);
-    const salopeInDegrees: number =  (salopeInRadian * 180) / Math.PI;
-
-    // change the css of the line
-    this.width = distance + 'px';
-    this.top = yMid + 'px';
-    this.left = xMid - (distance / 2) + 'px';
-    this.transform = 'rotate(' + salopeInDegrees + 'deg)';
+  private linesGoingAroundGlobeFromEast() {
+    return (this.connectionFrom === 'Manila' || this.connectionFrom === 'Tokyo' || this.connectionFrom === 'Sydney') &&
+      (this.connectionTo === 'San Francisco' || this.connectionTo === 'Los Angeles');
   }
-  private drawLine3(connectionFrom: Map<string, number>, connectionTo: Map<string, number>) {
-    const {height, width} = this.getMapSize();
-    const x1: number = connectionFrom.get('left');
-    const y1: number = connectionFrom.get('top');
-    const x2 = width - 12;
-    const y2: number = connectionTo.get('top');
 
-    // the distance between the tow points(for the line div width)
-    const distance: number = Math.sqrt( ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
-
-    // the mid-point between the two points, we use it as rotation center
-    const xMid: number = (x1 + x2) / 2 + 10;
-    const yMid: number = (y1 + y2) / 2 + 10;
-
-    // get the salope of the line between two points
-    const salopeInRadian: number = Math.atan2(y1 - y2, x1 - x2);
-    const salopeInDegrees: number =  (salopeInRadian * 180) / Math.PI;
-
-    // change the css of the line
-    this.width = distance + 'px';
-    this.top = yMid + 'px';
-    this.left = xMid - (distance / 2) + 'px';
-    this.transform = 'rotate(' + salopeInDegrees + 'deg)';
+  private linesGoingAroundGlobeFromWest() {
+    return (this.connectionFrom === 'San Francisco' || this.connectionFrom === 'Los Angeles') &&
+      (this.connectionTo === 'Manila' || this.connectionTo === 'Tokyo' || this.connectionTo === 'Sydney');
   }
+
   onResize() {
     this.createLine();
   }
